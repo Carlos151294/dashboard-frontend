5efc69652df62657cf51585260f02cb166d7a6b5
Carlos Flores <gbs@MacBook-Pro-de-Gabriela.local>
2020-07-19 21:51:00 -0500

    Last touch for dashboard. List of books added.

--- src/components/publicationList/publicationList.component.jsx
+++ src/components/publicationList/publicationList.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 import './publicationList.styles.scss';
 
 const PublicationList = () => {
+    const { publications } = useSelector(state => state.publications);
+
     return (
-        <div className="publications-container">Publication List</div>
+        <div>
+            <h1>List of available books</h1>
+            {
+                publications.map((item) => (
+                    <div className="book-item" key={item.id}>
+                        <div className="title">{item.title}</div>
+                        <div className="description">{item.description}</div>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
 
