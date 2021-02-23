40517795c72c1711046aecf00e7cea87ceaa923b
Carlos Flores Nava <carlos.flores.nava@hotmail.com>
2021-02-23 12:31:50 -0600

    Redux Devtools validation added (when browser doesn't have Redux Devtools extension)

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,8 @@ import thunk from 'redux-thunk';
 const enhancers = [];
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-    enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+if (process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+    enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__());
 }
 
 const store = createStore(rootReducer, compose(applyMiddleware(...middlewares), ...enhancers));
